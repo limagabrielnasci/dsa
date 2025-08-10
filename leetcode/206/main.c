bebcf858548600355248873e12cf6518775169e4
Gabriel Lima Nascimento <glnascimento@inf.ufpel.edu.br>
2025-08-10 11:36:00 -0300

    leetcode 206

---
+++ leetcode/206/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode* reverseList(struct ListNode* head) {
+    struct ListNode *prev = NULL, *curr = head, *next;
+    while (curr) {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
+
+struct ListNode* createNode(int val) {
+    struct ListNode* node = (struct ListNode*) malloc(sizeof(struct ListNode));
+    node->val = val;
+    node->next = NULL;
+    return node;
+}
+
+void printList(struct ListNode* head) {
+    while (head) {
+        printf("%d ", head->val);
+        head = head->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    return 0;
+}
